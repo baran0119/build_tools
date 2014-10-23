e82b9487f35e80ecb6ce932e7e7ee8e5699037a4
LuK1337 <priv.luk@gmail.com>
2014-10-23 21:45:08 +0200

    Update all patches

--- patch.sh
+++ patch.sh
@@ -1,5 +1,24 @@
-patch -p0 < 1.patch
-patch -p0 < 2.patch
-patch -p0 < 3.patch
-patch -p0 < 4.patch
-patch -p0 < 5.patch
+count=0
+curr_folder="$PWD"
+patches=(
+		'hardware/broadcom/libbt 0001-Add-BT-support-for-s2ve-s2vep.patch'
+		'external/chromium_org 0001-Fix-HWC-for-s2ve-s2vep.patch'
+		'frameworks/av 0001-FRAMEWORKS-AV-PATCH-for-s2ve-s2vep-device.patch'
+		'frameworks/native 0001-FRAMEWORKS-NATIVE-PATCH-for-s2ve-s2vep-device.patch'
+		'frameworks/base 0001-Led-patch-for-s2ve-s2vep.patch'
+		'packages/providers/MediaProvider 0001-PROVIDERS-MEDIA-MEDIAPROVIDER-PATCH-for-s2ve-s2vep-d.patch'
+		'vendor/pa 0001-Add-s2ve-p-to-vendor-pa.patch'
+	)
+
+while [ "x${patches[count]}" != "x" ]
+do
+	curr="${patches[count]}"
+	patch=`echo "$curr" | awk '{print $2}'`
+        folder=`echo "$curr" | awk '{print $1}'`
+
+	cd "$folder"
+	git apply "$curr_folder/$patch"
+	cd "$curr_folder"
+
+	count=$(( $count + 1 ))
+done
