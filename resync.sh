f2e8b0c0060e42d82dea19d80716aede0ac507e1
LuK1337 <priv.luk@gmail.com>
2014-11-25 16:39:30 -0500

    Update patches and support JAWA_HWC

--- resync.sh
+++ resync.sh
@@ -1,4 +1,4 @@
-repo init -u https://github.com/CyanogenMod/android.git -b cm-11.0
+repo init -u https://github.com/CyanogenMod/android.git -b cm-12.0
 repo forall -c 'git reset --hard ; git clean -fdx'
 
 # Sync repositories
