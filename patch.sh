2878e343894ad1d8f9f874e9bca0641e78efb2b6
LuK1337 <priv.luk@gmail.com>
2014-12-10 17:52:31 +0100

    Update and sort patches

--- patch.sh
+++ patch.sh
@@ -1,18 +1,19 @@
 count=0
 curr_folder="$PWD"
 patches=(
+                'external/icu external_icu.patch'
                 'frameworks/av frameworks_av.patch'
                 'frameworks/base frameworks_base.patch'
                 'frameworks/native frameworks_native.patch'
                 'frameworks/opt/net/wifi frameworks_opt_net_wifi.patch'
                 'frameworks/opt/telephony frameworks_opt_telephony.patch'
                 'hardware/broadcom/libbt hardware_broadcom_libbt.patch'
+                'packages/apps/AudioFX packages_apps_AudioFX.patch'
                 'packages/apps/Settings packages_apps_Settings.patch'
+                'packages/providers/MediaProvider packages_providers_MediaProvider.patch'
                 'packages/services/Telephony packages_services_Telephony.patch'
                 'system/core system_core.patch'
-		'external/icu external_icu.patch'
-		'packages/providers/MediaProvider packages_providers_MediaProvider.patch'
-		'vendor/cm vendor_cm.patch'
+                'vendor/cm vendor_cm.patch'
 	)
 
 while [ "x${patches[count]}" != "x" ]
