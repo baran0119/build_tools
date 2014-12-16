40709c6cc12b5cab4914c1dcb094c8f8cd0ce4ba
LuK1337 <priv.luk@gmail.com>
2014-12-16 18:30:35 +0100

    Update patches

--- patch.sh
+++ patch.sh
@@ -4,16 +4,16 @@ patches=(
                 'external/icu external_icu.patch'
                 'frameworks/av frameworks_av.patch'
                 'frameworks/base frameworks_base.patch'
+                'frameworks/base frameworks_base_led.patch'
                 'frameworks/native frameworks_native.patch'
                 'frameworks/opt/net/wifi frameworks_opt_net_wifi.patch'
                 'frameworks/opt/telephony frameworks_opt_telephony.patch'
                 'hardware/broadcom/libbt hardware_broadcom_libbt.patch'
-                'packages/apps/AudioFX packages_apps_AudioFX.patch'
+		'packages/apps/Mms packages_apps_Mms.patch''
                 'packages/apps/Settings packages_apps_Settings.patch'
                 'packages/providers/MediaProvider packages_providers_MediaProvider.patch'
                 'packages/services/Telephony packages_services_Telephony.patch'
                 'system/core system_core.patch'
-                'vendor/cm vendor_cm.patch'
 	)
 
 while [ "x${patches[count]}" != "x" ]
