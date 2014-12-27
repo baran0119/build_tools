ed9cfbb70c5c97f0b42c3625766f49fceff8b30b
baran0119 <baran0119@gmail.com>
2014-12-27 19:37:25 +0100

    patch.sh: miss with prev commit

--- patch.sh
+++ patch.sh
@@ -9,11 +9,5 @@ for PATCH in $(ls patches|grep .patch); do
 	git apply $CUR_DIR/patches/$PATCH
 	cd $CUR_DIR
 done
-for PATCH in $(ls patches/msim|grep .patch); do
-	PATH2PATCH=`echo $PATCH|cut -d. -f1|sed 's:_:/:g'`
-	echo "Patching $PATH2PATCH with $PATCH"
-	cd $PATH2PATCH
-	git apply $CUR_DIR/patches/msim/$PATCH
-	cd $CUR_DIR
-done
+
 echo "Patched"
